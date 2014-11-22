1906cf1870af2d20af2bc42837f10c84b5aceefd
yababay <pochta@yababay.ru>
2014-11-22 19:30:46 +0400

    gauge

--- media-plus/js/cameras.js
+++ media-plus/js/cameras.js
@@ -2,6 +2,8 @@ define(function(){
 	return function(center){
 		$(center).css('background-image', 'none')
 		requireCss('media-plus/css/cameras.css')  
+		$(center).load('media-plus/cameras.html', function(){
+		}
 	}
 })
 
--- media-plus/js/conditioners.js
+++ media-plus/js/conditioners.js
@@ -2,6 +2,8 @@ define(function(){
 	return function(center){
 		$(center).css('background-image', 'none')
 		requireCss('media-plus/css/conditioners.css')  
+		$(center).load('media-plus/conditioners.html', function(){
+		}
 	}
 })
 
